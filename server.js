49b7ea7a3df764602117472cc700f66f869ebdac
rjacob <rahnarjacob@gmail.com>
2024-07-04 13:15:17 +0530

    Completed code for all five questions, haven't tested due to facing initialisation errors

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const action = prompt(`
     `)
 
 
+//Input logic
 //Checking for valid input: if action = Number && action > 0 && action < 5 then proceed
 //else return "Please enter a valid input"
 
@@ -43,35 +44,32 @@ function validInput(action) {
     if (parseInt(action) === Number && parseInt(action) > 0 && parseInt(action) < 5) {
         return true
     } else {
-        return false `Please enter a valid input`
+        return `Please enter a valid input`
     }
 }
 
-
-//Input logic
-
-if (action === "1") {
+if (action === "1" && validInput()) {
     const customerData = {
         customerName: prompt("Please enter customer name:"),
         customerAge: prompt("Please enter customer age:")
     }
     createCustomer(customerData)
-} else if (action === "2") {
+} else if (action === "2" && validInput()) {
     viewAllCustomers()        //function to display all
-} else if (action === "3") {
-    await viewAllCustomers() //display list of customers so far
+} else if (action === "3" && validInput()) {
+    viewAllCustomers() //display list of customers so far
     customerId = prompt("PLease enter the ID of the customer you want to edit:")    //accept customer id (as a prompt) to get to the customer object in question
     customerData = {        //display (action = 1) prompts and reassign them to the customer object
         customerName: prompt("Please enter the customer name for updation:"),
         customerAge: prompt("Please enter the customer age for updation:")
     }
     updateCustomer()        //function to update
-} else if (action === "4") {
-    await viewAllCustomers()  //display list of customers so far
+} else if (action === "4" && validInput()) {
+    viewAllCustomers()  //display list of customers so far
     customerId = prompt("Please enter the ID of the customer you want to delete")   //get customer id from user
     deleteCustomer()        //function to delete
-} else if (action === "5") {
-    await mongoose.connection.close()
+} else if (action === "5" && validInput()) {
+    mongoose.connection.close()
     process.exit()
 }
 
