54ea3ad30427ef6d4f066092bc1fc41c61e949ff
rjacob <rahnarjacob@gmail.com>
2024-07-04 12:50:58 +0530

    Completed code for all five questions, but haven't checked if working due to missing module error

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-//
+//Constants/Imports
 
 const dotenv = require("dotenv").config()
 const mongoose = require("mongoose")
 const customer = require("./models/crm.js")
 const prompt = require('prompt-sync')();
 
+//Variables
+
+let customerName
+let customerAge
+let customerId
+let customerData
 
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Mongoose is connected')                
+        console.log('Mongoose is connected')
     } catch (error) {
-        console.log(error)         
+        console.log(error)
     }
 }
 
@@ -29,35 +35,52 @@ const action = prompt(`
     Number of action to run: 
     `)
 
+
 //Checking for valid input: if action = Number && action > 0 && action < 5 then proceed
 //else return "Please enter a valid input"
 
-    if (action === "1") {
-        const customerData = {
-            name: prompt("Please enter customer name"),
-            age: prompt("Please enter customer age")
-        }
-        createCustomer(customerData)
-    } else if (action === "2") {
-        //function to display all
-        viewAllCustomers()
-//     } else if (action === "3") {
-//         //function to update
-//         //display list of customers so far
-//         //accept customer id (as a prompt) to get to the customer object in question
-//         //display (action = 1) prompts 
-//         //reassignt them to the customer object
-//     } else if (action === "4") {
-// //get customer id
-// //function to delete
-    } else if (action === "5") {
-        await mongoose.connection.close()
-        process.exit()
+function validInput(action) {
+    if (parseInt(action) === Number && parseInt(action) > 0 && parseInt(action) < 5) {
+        return true
+    } else {
+        return false `Please enter a valid input`
     }
+}
+
+
+//Input logic
+
+if (action === "1") {
+    const customerData = {
+        customerName: prompt("Please enter customer name:"),
+        customerAge: prompt("Please enter customer age:")
+    }
+    createCustomer(customerData)
+} else if (action === "2") {
+    viewAllCustomers()        //function to display all
+} else if (action === "3") {
+    await viewAllCustomers() //display list of customers so far
+    customerId = prompt("PLease enter the ID of the customer you want to edit:")    //accept customer id (as a prompt) to get to the customer object in question
+    customerData = {        //display (action = 1) prompts and reassign them to the customer object
+        customerName: prompt("Please enter the customer name for updation:"),
+        customerAge: prompt("Please enter the customer age for updation:")
+    }
+    updateCustomer()        //function to update
+} else if (action === "4") {
+    await viewAllCustomers()  //display list of customers so far
+    customerId = prompt("Please enter the ID of the customer you want to delete")   //get customer id from user
+    deleteCustomer()        //function to delete
+} else if (action === "5") {
+    await mongoose.connection.close()
+    process.exit()
+}
+
+
+//Functions
 
 const createCustomer = async (customerData) => {
     try {
-        const newCustomer = await Customer.create(customerData)
+        const newCustomer = await customer.create(customerData)
     } catch (err) {
         console.log(err)
     }
@@ -65,8 +88,25 @@ const createCustomer = async (customerData) => {
 
 const viewAllCustomers = async () => {
     try {
-        const allCustomers = await Customer.find({})
+        const allCustomers = await customer.find({})
     } catch (err) {
         console.log(err)
     }
 }
+
+const updateCustomer = async () => {
+    try {
+        const updatingCustomer = await customer.findByIdAndUpdate()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const deleteCustomer = async () => {
+    try {
+        const deletedCustomer = await customer.findByIdAndDelete(customerId)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
